7d3fefc072986e93c10b6d93e05d7ff59e384c1a
Rawda Tarek <45421348+RawdaTarek103@users.noreply.github.com>
2020-02-06 01:21:42 +0200

    write numbers to textbox

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -6,6 +6,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Window.Type;
 import java.awt.GridLayout;
+import java.awt.TextField;
 import java.awt.Component;
 import java.awt.Color;
 import java.awt.Toolkit;
@@ -54,6 +55,11 @@ public class Calculator {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	
+	
+	
+	
+	
 	private void initialize() {
 		frmCalculator = new JFrame();
 		frmCalculator.getContentPane().setBackground(new Color(33,33,33));
@@ -73,6 +79,7 @@ public class Calculator {
 		panel.setLayout(null);
 		
 		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField.setBounds(10, 0, 464, 70);
 		textField.setToolTipText("");
 		textField.setSelectionColor(Color.BLACK);
@@ -89,6 +96,8 @@ public class Calculator {
 		button_del.setBackground(new Color(44, 41, 42));
 		button_del.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(textField.getText().length() > 0)
+					textField.setText(textField.getText().substring(0, textField.getText().length()-1));
 			}
 		});
 		button_del.setFont(new Font("Tahoma", Font.PLAIN, 12));
@@ -101,6 +110,12 @@ public class Calculator {
 		button_0.setBounds(85, 305, 65, 45);
 		button_0.setBorder(null);
 		button_0.setBackground(new Color(44, 41, 42));
+		button_0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(textField.getText().length() != 0)
+					textField.setText(textField.getText()+"0");
+			}
+		});
 		frmCalculator.getContentPane().add(button_0);
 		
 		JButton button_1 = new JButton("1");
@@ -109,6 +124,11 @@ public class Calculator {
 		button_1.setBounds(10, 249, 65, 45);
 		button_1.setBorder(null);
 		button_1.setBackground(new Color(44, 41, 42));
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"1");
+			}
+		});
 		frmCalculator.getContentPane().add(button_1);
 		
 		JButton button_2 = new JButton("2");
@@ -117,6 +137,11 @@ public class Calculator {
 		button_2.setBounds(85, 249, 65, 45);
 		button_2.setBorder(null);
 		button_2.setBackground(new Color(44, 41, 42));
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"2");
+			}
+		});
 		frmCalculator.getContentPane().add(button_2);
 		
 		JButton button_3 = new JButton("3");
@@ -125,6 +150,11 @@ public class Calculator {
 		button_3.setBounds(160, 249, 65, 45);
 		button_3.setBorder(null);
 		button_3.setBackground(new Color(44, 41, 42));
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"3");
+			}
+		});
 		frmCalculator.getContentPane().add(button_3);
 		
 		JButton button_4 = new JButton("4");
@@ -133,6 +163,11 @@ public class Calculator {
 		button_4.setBounds(10, 193, 65, 45);
 		button_4.setBorder(null);
 		button_4.setBackground(new Color(44, 41, 42));
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"4");
+			}
+		});
 		frmCalculator.getContentPane().add(button_4);
 		
 		JButton button_5 = new JButton("5");
@@ -141,6 +176,11 @@ public class Calculator {
 		button_5.setBounds(85, 193, 65, 45);
 		button_5.setBorder(null);
 		button_5.setBackground(new Color(44, 41, 42));
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"5");
+			}
+		});
 		frmCalculator.getContentPane().add(button_5);
 		
 		JButton button_6 = new JButton("6");
@@ -148,11 +188,12 @@ public class Calculator {
 		button_6.setForeground(new Color(255, 255, 255));
 		button_6.setBorder(null);
 		button_6.setBackground(new Color(44, 41, 42));
+		button_6.setBounds(160, 193, 65, 45);
 		button_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+						textField.setText(textField.getText()+"6");
 			}
 		});
-		button_6.setBounds(160, 193, 65, 45);
 		frmCalculator.getContentPane().add(button_6);
 		
 		JButton button_7 = new JButton("7");
@@ -161,6 +202,11 @@ public class Calculator {
 		button_7.setBounds(10, 137, 65, 45);
 		button_7.setBorder(null);
 		button_7.setBackground(new Color(44, 41, 42));
+		button_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"7");
+			}
+		});
 		frmCalculator.getContentPane().add(button_7);
 		
 		JButton button_8 = new JButton("8");
@@ -169,6 +215,11 @@ public class Calculator {
 		button_8.setBounds(85, 137, 65, 45);
 		button_8.setBorder(null);
 		button_8.setBackground(new Color(44, 41, 42));
+		button_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"8");
+			}
+		});
 		frmCalculator.getContentPane().add(button_8);
 		
 		JButton button_9 = new JButton("9");
@@ -176,11 +227,12 @@ public class Calculator {
 		button_9.setForeground(new Color(255, 255, 255));
 		button_9.setBorder(null);
 		button_9.setBackground(new Color(44, 41, 42));
+		button_9.setBounds(160, 137, 65, 45);
 		button_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"9");
 			}
 		});
-		button_9.setBounds(160, 137, 65, 45);
 		frmCalculator.getContentPane().add(button_9);
 		
 		JButton button_dot = new JButton(".");
@@ -189,17 +241,25 @@ public class Calculator {
 		button_dot.setBounds(160, 305, 65, 45);
 		button_dot.setBorder(null);
 		button_dot.setBackground(new Color(44, 41, 42));
+		button_dot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+".");
+			}
+		});
 		frmCalculator.getContentPane().add(button_dot);
 		
-		
-		
-		JButton btnX = new JButton("\u00D7");
-		btnX.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnX.setForeground(new Color(255, 255, 255));
-		btnX.setBounds(235, 137, 65, 45);
-		btnX.setBorder(null);
-		btnX.setBackground(new Color(28,69,82));
-		frmCalculator.getContentPane().add(btnX);
+		JButton button_mul = new JButton("\u00D7");
+		button_mul.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button_mul.setForeground(new Color(255, 255, 255));
+		button_mul.setBounds(235, 137, 65, 45);
+		button_mul.setBorder(null);
+		button_mul.setBackground(new Color(28,69,82));
+		button_mul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
+		frmCalculator.getContentPane().add(button_mul);
 		
 		JButton button_plus = new JButton("+");
 		button_plus.setFont(new Font("Tahoma", Font.PLAIN, 12));
@@ -207,6 +267,11 @@ public class Calculator {
 		button_plus.setBounds(235, 249, 65, 45);
 		button_plus.setBorder(null);
 		button_plus.setBackground(new Color(28,69,82));
+		button_plus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
 		frmCalculator.getContentPane().add(button_plus);
 		
 		JButton button_eq = new JButton("=");
@@ -215,6 +280,11 @@ public class Calculator {
 		button_eq.setBounds(235, 305, 65, 45);
 		button_eq.setBorder(null);
 		button_eq.setBackground(new Color(28,69,82));
+		button_eq.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//textField.setText();
+			}
+		});
 		frmCalculator.getContentPane().add(button_eq);
 		
 		JButton button_tan = new JButton("Tan");
@@ -224,6 +294,7 @@ public class Calculator {
 		button_tan.setBackground(new Color(28,69,82));
 		button_tan.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//
 			}
 		});
 		button_tan.setBounds(10, 81, 65, 45);
@@ -235,6 +306,11 @@ public class Calculator {
 		button_fact.setBounds(85, 81, 65, 45);
 		button_fact.setBorder(null);
 		button_fact.setBackground(new Color(28,69,82));
+		button_fact.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
 		frmCalculator.getContentPane().add(button_fact);
 		
 		JButton button_pow = new JButton("^");
@@ -243,22 +319,37 @@ public class Calculator {
 		button_pow.setBounds(160, 81, 65, 45);
 		button_pow.setBorder(null);
 		button_pow.setBackground(new Color(28,69,82));
+		button_pow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
 		frmCalculator.getContentPane().add(button_pow);
 		
-		JButton button_17 = new JButton("\u00F7");
-		button_17.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button_17.setForeground(new Color(255, 255, 255));
-		button_17.setBounds(235, 193, 65, 45);
-		button_17.setBorder(null);
-		button_17.setBackground(new Color(28,69,82));
-		frmCalculator.getContentPane().add(button_17);
+		JButton button_div = new JButton("\u00F7");
+		button_div.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button_div.setForeground(new Color(255, 255, 255));
+		button_div.setBounds(235, 193, 65, 45);
+		button_div.setBorder(null);
+		button_div.setBackground(new Color(28,69,82));
+		button_div.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
+		frmCalculator.getContentPane().add(button_div);
 		
-		JButton button_Cl = new JButton("CL");
-		button_Cl.setBorder(null);
-		button_Cl.setForeground(new Color(255, 255, 255));
-		button_Cl.setBackground(new Color(28,69,82));
-		button_Cl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button_Cl.setBounds(235, 81, 65, 45);
-		frmCalculator.getContentPane().add(button_Cl);
+		JButton button_CL = new JButton("CL");
+		button_CL.setBorder(null);
+		button_CL.setForeground(new Color(255, 255, 255));
+		button_CL.setBackground(new Color(28,69,82));
+		button_CL.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		button_CL.setBounds(235, 81, 65, 45);
+		button_CL.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText("");
+			}
+		});
+		frmCalculator.getContentPane().add(button_CL);
 	}
 }
